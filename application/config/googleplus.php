2656695b84dc5ba9b5077202cd398fbb316f6453
Ridha Khemiri <khemiri.ridha2013@gmail.com>
2017-03-22 23:22:18 +0100

    initialisation du projet

---
+++ application/config/googleplus.php
@@ -0,0 +1,11 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$config['googleplus']['application_name'] = 'test';
+$config['googleplus']['client_id']        = '856938909624-7to5jnd03kk47ffvklf4d546fq9m5qm8.apps.googleusercontent.com';
+$config['googleplus']['client_secret']    = 'VQB4xXokq56bXH6odDpLXNQ0';
+$config['googleplus']['redirect_uri']     = 'http://localhost/google-login/';
+$config['googleplus']['api_key']          = '';
+$config['googleplus']['scopes']           = array();
+
